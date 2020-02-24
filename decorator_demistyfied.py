d9864e06f2de0caec16f7ed76f71b4aa30f015d6
alxfed <alxfed@gmail.com>
2020-02-24 17:29:23 -0600

    adding objects to a set

--- decorator_demistyfied.py
+++ decorator_demistyfied.py
@@ -3,7 +3,7 @@
 """
 def print_args(function):
      def wrapper(*args, **kwargs):
-         print('Arguments:', args, kwargs) # this is the additional functionality the whole store has been started for
+         print('Arguments:', args, kwargs) # this is the additional functionality the whole story has been started for
          return function(*args, **kwargs)
      return wrapper
 
